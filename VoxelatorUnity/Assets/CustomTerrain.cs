9d4d2e3c6eb8d3936f3c1927ab57796410994ced
mobra <zagni.m@gmail.com>
2013-12-06 23:58:35 +0100

    let me commit, even if it's not finished

--- VoxelatorUnity/Assets/CustomTerrain.cs
+++ VoxelatorUnity/Assets/CustomTerrain.cs
@@ -15,6 +15,15 @@ public class CustomTerrain : MonoBehaviour
 	public float frequency = 40f;
 	public float amplitude = 1f;
 
+	public Color32[] terrainColor = new Color32[]{
+		new Color32(0, 0, 128, 1),
+		new Color32(0, 128, 255, 1 ), 
+		new Color32(240, 240, 64, 1),
+		new Color32(32, 160, 0, 1),
+		new Color32(224, 224, 0, 1),
+		new Color32(128, 128, 128, 1),
+		new Color32(255, 255, 255, 1)
+	};
 
 	private SimplexNoise3D simpleNoise;
 	private PerlinNoise perlinNise;
@@ -38,6 +47,7 @@ public class CustomTerrain : MonoBehaviour
 				
 				GRIDCELL[,,] grid;
 				MarchingCubes.FillVoxelData(Fill2DNoise, clasterOffset, clusterSize, gridScale, out grid);
+				MarchingCubes.ColorProcessor(AddColors);
 				
 				Vector3[] vertices;
 				int[] indices;
@@ -63,6 +73,35 @@ public class CustomTerrain : MonoBehaviour
 				(z - clusterSize / 2f) * (z - clusterSize / 2f) <= clusterSize * clusterSize / 4f? 1f : -1f;
 	}
 
+	public Color32 AddColors(float height)
+	{
+		if (height == terrainMinHeight)
+			return terrainColor[0];
+
+		else if (height > terrainMinHeight && height < (terrainMaxHeight - terrainMinHeight) / 100*10)
+			return terrainColor[1];
+
+		else if (height > (terrainMaxHeight - terrainMinHeight) / 100*10 && height < (terrainMaxHeight - terrainMinHeight) / 100*25)
+			return terrainColor[2];
+
+		else if (height > (terrainMaxHeight - terrainMinHeight) / 100*25 && height < (terrainMaxHeight - terrainMinHeight) / 100*50)
+			return terrainColor[3];
+
+		else if (height > (terrainMaxHeight - terrainMinHeight) / 100*50 && height < (terrainMaxHeight - terrainMinHeight) / 100*70)
+			return terrainColor[4];
+
+		else if (height > (terrainMaxHeight - terrainMinHeight) / 100*70 && height < (terrainMaxHeight - terrainMinHeight) / 100*90)
+			return terrainColor[5];
+
+		else if (height > (terrainMaxHeight - terrainMinHeight) / 100*90 && height < terrainMaxHeight)
+			return terrainColor[6];
+
+		else
+			return new Color32(0,0,0,0);
+	
+
+
+	}
 	float FillRandom(object[] parameters)
 	{
 		return Random.value;
--- VoxelatorUnity/Assets/MarchingCubes.cs
+++ VoxelatorUnity/Assets/MarchingCubes.cs
@@ -24,15 +24,7 @@ public delegate float FillInVoxelsDataProcessor(object[] parameters);
 
 public class MarchingCubes  
 {
-	public Color32[] terrainColor = new Color32[]{
-		Color32(0, 0, 128),
-		Color32(0, 128, 255 ), 
-		Color32(240, 240, 64),
-		Color32(32, 160, 0),
-		Color32(224, 224, 0),
-		Color32(128, 128, 128),
-		Color32(255, 255, 255)
-	};
+
 	//marching cubes table data
 	
 	// ** my comments ** //
@@ -404,6 +396,11 @@ public class MarchingCubes
 		}
 	}
 
+//	public static void ColorProcessor(Vector3 height)
+//	{
+//		if (height == )
+//	}
+
 	public static void GenerateChunk(GRIDCELL[,,] voxelsData, bool optimized, out int[] indices, out Vector3[] vertices) 
 	{
 		int lastIndex = 0;
@@ -425,6 +422,17 @@ public class MarchingCubes
 					int CubeIndex = 0;
 								
 					Vector3[] VertexList = new Vector3[12];
+
+//					Color32[] terrainColor = new Color32[]{
+//						Color32(0, 0, 128),
+//						Color32(0, 128, 255 ), 
+//						Color32(240, 240, 64),
+//						Color32(32, 160, 0),
+//						Color32(224, 224, 0),
+//						Color32(128, 128, 128),
+//						Color32(255, 255, 255)
+//					};
+
 
 					for(int i = 0; i < 8; i++) 
 					{
@@ -578,7 +586,7 @@ public class MarchingCubes
 								// The trong problems is that I don't try to share those vertices
 								// that have been created inside of current voxel
 								totalVertices.Add(VertexList[localVertIndex]);
-								totalColors.Add(terrainc());
+								totalColors.Add(AddColors(voxelsData[x,y,z].position.y));
 
 								lastIndex = totalVertices.Count;
                             }
